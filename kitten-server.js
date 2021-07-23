abc5aa08fe9c0d6146c836fa2efcdf5e2395bd1d
aid_hak <aid_hakiqi@hotmail.com>
2021-07-23 16:07:09 +0200

    minor corrections

--- kitten-server.js
+++ kitten-server.js
@@ -4,31 +4,30 @@ const port = 3000
 const sharp = require('sharp')
 const fs = require('fs')
 
-function notRandom(height,width){
+function notRandom(height, width) {
     const files = fs.readdirSync(__dirname + "/jpg-low").filter(filename => filename.endsWith("jpg"))
     const result = height * width
     const index = result % files.length
     return (files[index])
 }
 
-function resize(pathname,output,height,width)
-{
+function resize(pathname, output, height, width) {
     let newheight = parseInt(height)
     let newwidth = parseInt(width)
     return sharp(pathname).resize({height: newheight, width: newwidth}).toFile(output)
 }
+
 app.get('/:height/:width', (req, res) => {
     const height = req.params.height
     const width = req.params.width
-    const fonjaPic = notRandom(height,width)
-    let path = __dirname +"/jpg-low/" + fonjaPic
-    let output = __dirname + "/jpg-low/jpg-resize/"+height+"x"+width
+    const fonjaPic = notRandom(height, width)
+    let path = __dirname + "/jpg-low/" + fonjaPic
+    let output = __dirname + "/jpg-low/jpg-resize/" + height + "x" + width
 
-    if(fs.existsSync(output)){
+    if (fs.existsSync(output)) {
         res.header("content-type", "image/jpg")
         res.sendFile(output)
-    }
-    else {
+    } else {
         resize(path, output, height, width).then(function () {
             res.header("content-type", "image/jpg")
             res.sendFile(output)
